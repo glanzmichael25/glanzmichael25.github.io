07958bd111f5d15d3e9aa4ad4a9816c53e9ff115
glanzmichael25 <glanz.michael.25@gmail.com>
2019-01-14 01:27:33 -0800

    reworked files including javascript

---
+++ javascript/timestamplogging.js
@@ -0,0 +1,10 @@
+
+function f() {
+    var currentdate = new Date();
+    var datetime = "Last Sync: " + currentdate.getDay() + "/"+currentdate.getMonth()
+        + "/" + currentdate.getFullYear() + " @ "
+        + currentdate.getHours() + ":"
+        + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+    console.log(currentdate);
+    alert(currentdate);
+}
